e64652f7cc8be253a3929729bd9c7e2ad5e38a6c
Robbie Ferguson <contact@category5.tv>
2016-09-12 16:35:53 -0400

    Special

--- init.lua
+++ init.lua
@@ -10,3 +10,17 @@ local function poop(player)
 	local pname = minetest.get_player_by_name(player)
 	minetest.chat_send_player(player, pname .. " pooped.")
 end
+
+
+minetest.register_privilege("special", "A truly special player.")
+
+minetest.register_chatcommand("special", {
+	description = "Test if you are special.",
+	privs = {special=true},
+	func = special
+})
+
+local function special(player)
+	local pname = minetest.get_player_by_name(player)
+	minetest.chat_send_player(player, pname .. " is special.")
+end
